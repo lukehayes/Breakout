325ce8a32c3e203668d1c812b8f44b04a68761ce
lukehayes <lukepurplehayes@gmail.com>
2021-01-09 19:18:30 +0000

    Added basic ball collision detection for walls

--- include/Ball.h
+++ include/Ball.h
@@ -1,6 +1,12 @@
 #ifndef _BALL_H
 #define _BALL_H
 
+static const int SPEED = 5;
+static const int x_MIN_BOUNDS = 2;
+static const int x_MAX_BOUNDS = 780;
+static const int y_MIN_BOUNDS = 2;
+static const int y_MAX_BOUNDS = 590;
+
 typedef struct Ball 
 {
     int x;
@@ -14,6 +20,23 @@ typedef struct Ball
 
 void ballUpdate(Ball* ball)
 {
+    if( ball->x < x_MIN_BOUNDS | ball->x > x_MAX_BOUNDS )
+    {
+        ball->xDir = -ball->xDir;
+
+        ball->y = 100;
+
+        printf("x Flip \n");
+    }
+
+    if(ball->y < y_MIN_BOUNDS | ball->y > y_MAX_BOUNDS)
+    {
+        ball->yDir = -ball->yDir;
+        printf("y Flip \n");
+    }
+
+    ball->x += ball->xDir * SPEED;
+    ball->y += ball->yDir * SPEED;
 }
 
 
